3a7d99d7a4ad12ed92263ee9db609948e9d0ea24
Viswanadh <viswanadh.n@krify.com>
2024-07-11 17:32:02 +0530

    Initial Commit

---
+++ Mac-Sample-Groceries-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mac-Sample-Groceries-App
+//
+//  Created by KSMACMINI-016 on 11/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mac-Sample-Groceries-App/Mac_Sample_Groceries_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Sample_Groceries_AppApp.swift
+//  Mac-Sample-Groceries-App
+//
+//  Created by KSMACMINI-016 on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Sample_Groceries_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
